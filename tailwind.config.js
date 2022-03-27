259e1c20f97e74a49c1cdab2955218ac3cf6656d
frank-chris <chrisfrancischris@gmail.com>
2022-03-27 06:56:35 +0530

    Add basic UI

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.html", "./*.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter var'],
+    }
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
